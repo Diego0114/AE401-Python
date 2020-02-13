1d45e02f0bbf62d8990f0f715349c1427ba4c116
Diego0114 <60870243+Diego0114@users.noreply.github.com>
2020-02-13 13:09:07 +0800

    猜數字(1~20)

---
+++ L3 Home work.py
@@ -0,0 +1,17 @@
+import random
+a=random.randint(1,20)
+c=0
+while c<5:
+    b=input("隨機取數1~20(猜五次):")
+    c=c+1
+    if int(b)>a:
+        print("太大了")
+    elif int(b)<a:
+        print("太小了")
+    else:
+        print("你答對了")
+        print("玩了",c,"次")
+        break
+if c==5:
+    print("game over")
+    
