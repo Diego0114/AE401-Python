c6a2d2a5a4be20472784c9d8d2e10507869d1edc
Diego0114 <60870243+Diego0114@users.noreply.github.com>
2020-02-18 14:45:17 +0800

    分數進階版

--- L6 Home work.py
+++ L6 Home work.py
@@ -1,17 +1,32 @@
-num=0
-big=0
-min=100
-scores=[["a",0],["b",1],["c",2],["d",3],["e",4]]
-for i in range(len(scores)):
-    if scores[i][1]>big:
-        big=scores[i][1]
-        bigname=scores[i][0]
-    if scores[i][1]<min:
-        min=scores[i][1]
-        minname=scores[i][0]
-    num+=scores[i][1]
-print("最高分:",big)
-print("最高分名字:",bigname)
-print("最低分:",min)
-print("最低分名字:",minname)
-print("平均",num/len(scores))
+num=int(input("班上人數:"))#輸入人數
+slist=[]#設一個要儲存名字和分數的序列
+for i in range(num):
+    score=int(input("成績"))#輸入成績
+    name=input("名字")#輸入名字
+    slist.append(score)
+    slist.append(name)
+def ave (slist):#平均的函式
+    t=0
+    for j in range(0,num*2,2):
+        t=t+(slist[j])
+    return t#回傳
+def maxx (slist):#最高分分數和最高分名字的函式
+    maxxx=0
+    for j in range(0,num*2,2):
+        if slist[j]>=maxxx:
+            maxxx=slist[j]
+            maxxname=slist[j+1]
+    print("最高分分數:",maxxx)#印出最高分分數
+    print("最高分名字:",maxxname)#印出最高分名字
+def minn (slist):#最低分分數和最低分名字的函式
+    minnn=100
+    for j in range(0,num*2,2):
+        if slist[j]<=minnn:
+            minnn=slist[j]
+            minnname=slist[j+1]
+    print("最低分分數:",minnn)#印出最低分分數
+    print("最低分名字:",minnname)#印出最低分名字
+tt=ave(slist)#創一個變數來接t
+print("平均:",tt)#印出平均
+maxx(slist)#召喚最高分分數和最高分名字的函式
+minn(slist)#召喚最低分分數和最低分名字的函式
